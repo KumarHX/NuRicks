6cb1f866a3a0769e3ab3e5c759aa3deaf32bd006
Pranav Kumar <Pranav98@gmail.com>
2017-05-22 22:39:47 -0700

    pushing

--- Backend/NuRicks/index.js
+++ Backend/NuRicks/index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 // Helmet defaults
 app.use(helmet());
 app.use(cors({
-    origin: "http://localhost:8001",
+    origin: "https://aaruel.github.io/",
     credentials: true
 }));
 
