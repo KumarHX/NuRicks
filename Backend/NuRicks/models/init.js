84c6a9b73b9491e1e50c276e11b6bad15d29fdf9
Pranav Kumar <Pranav98@gmail.com>
2017-05-22 20:52:44 -0700

    debug

--- Backend/NuRicks/models/init.js
+++ Backend/NuRicks/models/init.js
@@ -10,11 +10,32 @@ var braintree = require("braintree");
 
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('nuricks', 'root', '11fire', {
+//var sequelize = new Sequelize('nuricks', 'root', '11fire', {
+  //  host: 'localhost',
+    //port: 3306,
+    //dialect: 'mysql'
+//});
+
+console.log(process.env.DATABASE_URL);
+
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  console.log("hello");
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'mysql',
+    logging:  true //false
+  });
+} 
+else {
+	console.log("hello233333333");
+  // the application is executed on the local machine
+  sequelize = new Sequelize('nuricks', 'root', '11fire', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql'
 });
+}
+
 
 var gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
